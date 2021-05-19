a5d4584722ad4cbd9160da2e9971ef23434dc9f7
Alen Zeinolov <github@alenzeinolov.com>
2021-05-19 23:22:45 +0600

    Fix binary search

--- src/binary_search.py
+++ src/binary_search.py
@@ -3,10 +3,9 @@ from typing import List
 
 def binary_search(arr: List[int], n: int):
     low = 0
-    high = len(arr) // 2
+    high = len(arr) - 1
     while (low <= high):
         mid = (high + low) // 2
-
         if arr[mid] < n:
             low = mid + 1
         elif arr[mid] > n:
@@ -16,7 +15,7 @@ def binary_search(arr: List[int], n: int):
     return None
 
 def main():
-    print(binary_search([1, 3, 5, 7, 9], 5))
+    print(binary_search([1, 3, 5, 7, 9, 11, 13, 15, 17, 19], 7))
 
 if __name__ == '__main__':
     main()
